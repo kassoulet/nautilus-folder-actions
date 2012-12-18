7e3131506712c1b3e00cc2ac6113284c6b3f39db
Gautier Portet <kassoulet@gmail.com>
2012-12-18 10:38:11 +0100

    Update run.sh
    Create nautilus extensions folder.

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 mkdir /tmp/nautilus-test
+mkdir -p ~/.nautilus/python-extensions/
 cp nautilus-folder-actions.py ~/.nautilus/python-extensions/
 killall -9 nautilus
 TMPDIR=/tmp/nautilus-test nautilus --no-desktop .
